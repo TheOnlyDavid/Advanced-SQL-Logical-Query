ae3e4c2e0c7902b41f3627b8a106b1410d9f0e4c
David Aarón <aaron_david6@hotmail.com>
2022-03-14 11:25:01 -0700

    Update 4.- Set operator.sql

--- 4.- Set operator.sql
+++ 4.- Set operator.sql
@@ -13,3 +13,15 @@ SELECT	Name, Species
 FROM	Vaccinations
 GROUP BY Name, Species
 HAVING	COUNT(*) > 1;
+
+-- The elegant solution, Breeds that were never adopted
+SELECT	Species, Breed
+FROM	Animals
+EXCEPT	
+SELECT	AN.Species, AN.Breed 
+FROM	Animals AS AN
+		INNER JOIN
+		Adoptions AS AD
+		ON	AN.Species = AD.Species
+			AND
+			AN.Name = AD.Name;
